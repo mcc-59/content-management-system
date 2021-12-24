eb3e218d4bb8fea53f0e2f7a7c0e8b5b12bc107f
Akhmad Mumtaz <akhmadmumtaz@gmail.com>
2021-12-24 17:24:10 +0700

    add service controller
    role and user

---
+++ src/main/java/com/id/mii/backend/cms/controller/RoleController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.id.mii.backend.cms.controller;
+
+import com.id.mii.backend.cms.model.Role;
+import com.id.mii.backend.cms.service.RoleService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author akhma
+ */
+
+@RestController
+@RequestMapping("/role")
+public class RoleController {
+    private RoleService roleService;
+    
+    @Autowired
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<Role>> getAll(){
+        return new ResponseEntity(roleService.getAll(), HttpStatus.OK);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Role> getById(@PathVariable("id") Long id){
+        return new ResponseEntity(roleService.getById(id), HttpStatus.OK);
+    }
+    
+    @PostMapping
+    public ResponseEntity<Role> create(@RequestBody Role role){
+        return new ResponseEntity(roleService.create(role), HttpStatus.CREATED);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Role> update(@PathVariable("id") Long id, @RequestBody Role role) {
+        return new ResponseEntity(roleService.update(id, role), HttpStatus.OK);
+    }
+    
+    
+}
---
+++ src/main/java/com/id/mii/backend/cms/service/TokenService.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.id.mii.backend.cms.service;
+
+import com.id.mii.backend.cms.model.Token;
+import com.id.mii.backend.cms.model.User;
+import com.id.mii.backend.cms.repository.TokenRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ *
+ * @author akhma
+ */
+
+@Service
+public class TokenService {
+    private TokenRepository tokenRepository;
+    
+    @Autowired
+    public TokenService(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+    
+    public List<Token> getAll(){
+        return tokenRepository.findAll();
+    }
+    
+    public Token getByTokenCode(String tokenCode){
+        return tokenRepository.findByTokenCode(tokenCode)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Token Code Not Found"));
+    }
+    
+    public Token create(User user, int dayExpired){
+        UUID uuid = UUID.randomUUID();
+        
+        LocalDateTime created = LocalDateTime.now();
+        LocalDateTime expired = LocalDateTime.now().plusDays(dayExpired);
+        
+        Token token = Token.builder().tokenCode(uuid.toString()).user(user)
+                .created(created).expired(expired).build();
+        
+        return tokenRepository.save(token);
+    }
+    
+    
+}
