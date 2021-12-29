4735224d3002366f277aa049a75a56acfd076eac
Akhmad Mumtaz <akhmadmumtaz@gmail.com>
2021-12-29 09:32:42 +0700

    initialize frontend

---
+++ src/main/java/com/id/mii/frontend/cms/ClientSideCmsApplication.java
@@ -0,0 +1,13 @@
+package com.id.mii.frontend.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSideCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSideCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/id/mii/frontend/cms/ClientSideCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.id.mii.frontend.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSideCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
