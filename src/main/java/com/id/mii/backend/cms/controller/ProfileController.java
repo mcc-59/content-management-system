e72f9ea07eee1677a6601ce2f932e1d70d4c5af6
Akhmad Mumtaz <akhmadmumtaz@gmail.com>
2022-01-02 17:49:08 +0700

    add activate account system

--- src/main/java/com/id/mii/backend/cms/controller/ProfileController.java
+++ src/main/java/com/id/mii/backend/cms/controller/ProfileController.java
@@ -10,6 +10,7 @@ import com.id.mii.backend.cms.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,9 +35,15 @@ public class ProfileController {
     }
     
     @PutMapping("/{tokenCode}")
-    @PreAuthorize("hasAuthority('READ_DATA')")
+//    @PreAuthorize("hasAuthority('READ_DATA')")
     public User updatePass(@PathVariable ("tokenCode") String code,  @RequestBody User user){
         return userService.updatePassword(code, user);
+    }
+    
+    @GetMapping("/{tokenCode}")
+//    @PreAuthorize("hasAuthority('READ_DATA')")
+    public User activate(@PathVariable ("tokenCode") String code){
+        return userService.activate(code);
     }
     
     
