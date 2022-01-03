bde65d2a95fbc8f03a60996dab277e4b2f3f3237
alimustofa-am <74023907+alimustofa-am@users.noreply.github.com>
2022-01-03 08:46:41 +0700

    dashboard layout

---
+++ src/main/java/com/id/mii/frontend/cms/config/TemplateEngineConfig.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.id.mii.frontend.cms.config;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author sihlihis
+ */
+@Configuration
+public class TemplateEngineConfig {
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+}
