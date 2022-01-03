26047100f7fd0e18b721026e6be52ec1d6aed08e
Akhmad Mumtaz <akhmadmumtaz@gmail.com>
2022-01-03 14:14:41 +0700

    add content-home

--- src/main/java/com/id/mii/backend/cms/repository/ContentRepository.java
+++ src/main/java/com/id/mii/backend/cms/repository/ContentRepository.java
@@ -6,7 +6,9 @@
 package com.id.mii.backend.cms.repository;
 
 import com.id.mii.backend.cms.model.Content;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,4 +18,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long>{
     
+    @Query(value = "SELECT * FROM tb_content AS c WHERE c.id IN (SELECT content_id FROM tb_content_category WHERE category_id = ?1) ORDER BY publish_date;", nativeQuery = true)
+    List<Content> findContentByCategory(Long categoryId);
+    
+    @Query(value = "SELECT * FROM tb_content WHERE views = (SELECT MAX(views) FROM tb_content) LIMIT 1;", nativeQuery = true)
+    Content findTrendingContent();
+    
+    @Query(value = "SELECT * FROM tb_content ORDER BY publish_date LIMIT 10;", nativeQuery = true)
+    List<Content> findFirstTen();
 }
+    
